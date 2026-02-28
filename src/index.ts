48b9da43e0ae610b26a1d1df0dfd60462d154e43
HeathKnowles <yyashuday13@gmail.com>
2026-02-28 14:31:27 +0530

    Added port based on environment variables

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import "dotenv/config"
 import { serve } from "bun"
 import identifyRoute from "./routes/identify"
 
+const port = Number(process.env.port) || 3000
+
 serve({
-  port: 3000,
+  port,
   fetch: identifyRoute.fetch,
 })
