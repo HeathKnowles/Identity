d66df244c80838ecb2694711d562e1ff85f85e13
HeathKnowles <yyashuday13@gmail.com>
2026-02-28 14:28:16 +0530

    prisma postgres integration

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined")
 }
 
-const pool = new pg.Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-  max: 5,
 })
-
-const adapter = new PrismaPg(pool)
 
 export const prisma = new PrismaClient({
   adapter,
